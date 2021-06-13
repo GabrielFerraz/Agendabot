4cd1b8f9fcb4557828ffc43f0f53cdf13f9c63b8
Gabriel Ferraz <gabrielmaiaferraz@gmail.com>
2021-06-13 15:35:45 -0300

    Added allow repeat command

---
+++ src/commands/allow.repeat.command.ts
@@ -0,0 +1,17 @@
+import { Message } from "discord.js";
+import { Bot } from "../bot/bot";
+import { allowed } from "../helpers/config";
+import container from "../inversify.config";
+import { TYPES } from "../types/types";
+import { BaseCommand } from "./base.command";
+
+export class AllowCommand extends BaseCommand {
+  static command: string = "permitir-repeticao";
+
+  static async run(message: Message) {
+    let bot = container.get<Bot>(TYPES.Bot);
+    // return message.reply("Pong!");
+    console.log(bot.allowed);
+    bot.allowed = !allowed;
+  }
+}
